99ff6e05309ba3e1c11e0bc4022a2577ca5026d7
Daniel Saiz <dsaiztc@gmail.com>
2016-10-25 19:42:07 +0100

    Adding test for TimeoutError try-except workflow

--- timeout/tests/test_timeout.py
+++ timeout/tests/test_timeout.py
@@ -2,6 +2,7 @@ from timeout import timeout, TimeoutError
 import time
 import pytest
 
+
 def test_timeout():
     @timeout(1)
     def my_task():
@@ -9,3 +10,19 @@ def test_timeout():
 
     with pytest.raises(TimeoutError) as excinfo:
         my_task()
+
+
+def test_tryexcept():
+    @timeout(1)
+    def exception_catched():
+        try:
+            time.sleep(1.5)
+        except TimeoutError:
+            print('TimeoutError catched within the function')
+            assert False
+
+    try:
+        exception_catched()
+    except TimeoutError:
+        print('TimeoutError catched outside the function')
+        assert True
